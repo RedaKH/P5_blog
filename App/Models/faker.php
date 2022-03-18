fee6f4c640dfd3f7c722fb2520fdb716d98ce017
RedaKH <reda.khaldi@gmail.com>
2022-03-18 22:50:40 +0100

    featuretest2

--- App/Models/faker.php
+++ App/Models/faker.php
@@ -4,13 +4,15 @@ require_once 'vendor/autoload.php';
 
     $faker= Faker\Factory::create();
     $pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', 'root');
-    $sql = "INSERT INTO comment ( content,time) VALUES (?,?)";
+    $sql = "INSERT INTO post ( title,content,created_at) VALUES (?,?,?)";
     $stmt = $pdo->prepare($sql);
 $faker = \Faker\Factory::create();
 $insertedPKs = array();
 for ($i=0; $i < 100; $i++) {
-  $stmt->bindValue(1, $faker->text, PDO::PARAM_STR);
-  $stmt->bindValue(2, $faker->date, PDO::PARAM_STR);
+   $stmt->bindValue(1, $faker->text, PDO::PARAM_STR);
+
+  $stmt->bindValue(2, $faker->text, PDO::PARAM_STR);
+  $stmt->bindValue(3, $faker->date, PDO::PARAM_STR);
   $stmt->execute();
   $insertedPKs[]= $pdo->lastInsertId();
 
