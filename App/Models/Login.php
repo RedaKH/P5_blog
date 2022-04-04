4cfd751704df8a306bcf0ec9854ae02707235dca
RedaKH <reda.khaldi@gmail.com>
2022-04-04 16:25:32 +0200

    LOOOOOOGIIIIIIINDONE!

--- App/Models/Login.php
+++ App/Models/Login.php
@@ -22,7 +22,7 @@ class Login extends \Core\Model
     public static function canLogin($username, $password)
     {
         $db = static::getDB();
-        $query = $db->prepare('SELECT * FROM user WHERE username=? AND password=?');
+        $query = $db->prepare('SELECT * FROM user WHERE username=? AND password=? AND role=true');
         $query->bindValue(1,$username, PDO::PARAM_STR);
         $query->bindValue(2,$password, PDO::PARAM_STR);
         $query->execute();       
