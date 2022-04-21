f180ee110cb574c19c736ff47d081ea783c57031
RedaKH <37907191+RedaKH@users.noreply.github.com>
2022-04-21 13:26:23 +0200

    codacytest3

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -39,25 +39,25 @@ class Home extends \Core\Controller
     {
         // Check for empty fields
         if (
-            empty(htmlspecialchars($_POST['firstname'])) ||
-            empty(htmlspecialchars($_POST['lastname'])) ||
-            empty(htmlspecialchars($_POST['emailAddress'])) ||
-            empty(htmlspecialchars($_POST['message'])) ||
-            !filter_var(htmlspecialchars($_POST['emailAddress']), FILTER_VALIDATE_EMAIL)
+            empty($_POST['firstname']) ||
+            empty(($_POST['lastname']) ||
+            empty($_POST['emailAddress']) ||
+            empty($_POST['message']) ||
+            !filter_var($_POST['emailAddress'], FILTER_VALIDATE_EMAIL))
         ) {
             echo "No arguments Provided!";
             return false;
         }
 
-        $firstname = strip_tags(htmlspecialchars($_POST['firstname']));
-        $lastname = strip_tags(htmlspecialchars($_POST['lastname']));
-        $email_address = strip_tags(htmlspecialchars($_POST['emailAddress']));
-        $message = strip_tags(htmlspecialchars($_POST['message']));
+        $firstname = strip_tags($_POST['firstname']);
+        $lastname = strip_tags($_POST['lastname']);
+        $email_address = strip_tags($_POST['emailAddress']);
+        $message = strip_tags($_POST['message']);
 
 
 
         // Create the email and send the message
-        $to = 'raidenkratos@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+        $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
         $email_subject = "Website Contact Form:  $firstname $lastname";
         $email_body = "Vous avez reçu un nouveau message de votre formulaire de contact.\n\n" . "Voilà les détails:\n\n Prénom: $firstname\n\n Nom: $lastname\n\nEmail: $email_address\n\nMessage:\n$message";
         $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
