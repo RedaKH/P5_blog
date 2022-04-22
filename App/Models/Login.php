c7991c891acb5d388fe7b2fa38686297d7d6cd53
RedaKH <37907191+RedaKH@users.noreply.github.com>
2022-04-22 18:50:08 +0200

    .

--- App/Models/Login.php
+++ App/Models/Login.php
@@ -4,21 +4,13 @@ namespace App\Models;
 
 use PDO;
 
-/**
- * Example user model
- *
- * PHP version 7.0
- */
+
 class Login extends \Core\Model
 {
 
 
 
-    /**
-     * Get all the users as an associative array
-     *
-     * @return array
-     */
+   
     public static function canLogin($username, $password)
     {
         $db = static::getDB();
@@ -33,26 +25,7 @@ class Login extends \Core\Model
         }
     }
 
-    public function oldPasswordMatches($oldpassword)
-    {
-        $db = static::getDB();
-       $id = $_SESSION['id'];
-        $query = $db->prepare('SELECT * FROM user WHERE id=? AND password=?');
-        $query->bindValue(1, $id, PDO::PARAM_STR);
-        $query->bindValue(2, $oldpassword, PDO::PARAM_STR);
-        $query->execute();
-        if ($query->rowCount() > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public static function changePassword($id,$newpassword)
-    {
-        $db = static::getDB();
-        $sql = "UPDATE user SET password=?, WHERE id=?";
-        $stmt = $db->prepare($sql);
-        $stmt->execute([$newpassword,$id]);
-    }
+
+  
 }
